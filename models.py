760def9fab827147c6a50334c9f170cda31ceffb
dennissezambrano2017 <66701370+dennissezambrano2017@users.noreply.github.com>
2024-01-16 20:09:34 -0500

    Update models.py

--- models.py
+++ models.py
@@ -1,7 +1,13 @@
-import tensorflow_hub as hub
+import os
+import pathlib
+import tensorflow as tf
+def download_model(model_name):
+    base_url = 'http://download.tensorflow.org/models/object_detection/'
+    model_file = model_name + '.tar.gz'
+    model_dir = tf.keras.utils.get_file(fname=model_name,
+                                        origin=base_url + model_file,
+                                        untar=True)
+    return str(model_dir)
 
-# URL del modelo de MobileNet para detección de objetos
-model_url = "https://tfhub.dev/google/openimages_v4/ssd/mobilenet_v2/1"
-
-# Cargar el modelo
-model = hub.load(model_url)
+MODEL_NAME = 'ssd_mobilenet_v2_coco_2018_03_29'
+PATH_TO_MODEL_DIR = download_model(MODEL_NAME)
